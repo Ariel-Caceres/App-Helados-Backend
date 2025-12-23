2b212d1a448395ba420de4b8716b1e94ce24cf88
ariel <ariel10player@gmail.com>
2025-12-23 03:24:33 -0300

    fix: modificacion del config firestore

--- src/firebase/conifg.ts
+++ src/firebase/conifg.ts
@@ -1,11 +1,19 @@
 import admin from "firebase-admin";
 import 'dotenv/config';
 
-// 1. Cargamos el archivo JSON que bajaste de Firebase
-// Asegurate de que el archivo esté en la misma carpeta y se llame así
-const serviceAccount = require("./serviceAccountKey.json");
+// 1. Intentamos obtener la clave desde el entorno (para Render)
+// Si no existe, usamos el archivo local (para tu PC)
+let serviceAccount;
 
-// 2. Inicializamos solo una vez (evita errores de reinicio)
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    // Render leerá esto como un string, así que lo convertimos a objeto
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    // En tu PC, como no existe la variable, sigue usando el JSON
+    serviceAccount = require("./serviceAccountKey.json");
+}
+
+// 2. Inicializamos solo una vez
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount)
