579abf3576c2ec3f4c64eb8586a5ef461f98f9ba
ariel <ariel10player@gmail.com>
2025-12-28 20:25:00 -0300

    fix: import de expres en router

--- src/routes/ventas.routes.ts
+++ src/routes/ventas.routes.ts
@@ -1,12 +1,13 @@
-import Express from "express";
+import { Router } from "express";
 import { ventasController } from "../controllers/ventas.controller";
 
-export const router = Express.Router()
-router.get("/sales/:mes", ventasController.getAll)
-router.get("/sale/:id", ventasController.getById)
-router.post("/sell", ventasController.create)
-router.put("/edit/:id", ventasController.update)
-router.delete("/delete/:id", ventasController.delete)
+export const router = Router();
+
+router.get("/sales/:mes", ventasController.getAll);
+router.get("/sale/:id", ventasController.getById);
+router.post("/sell", ventasController.create);
+router.put("/edit/:id", ventasController.update);
+router.delete("/delete/:id", ventasController.delete);
 router.get("/", (_req, res) => {
     res.send("🔥 Backend funcionando en Render");
 });
