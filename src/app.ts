652610be4c50a5589322613169fe3fe3fb6cc918
ariel <ariel10player@gmail.com>
2025-12-29 03:30:05 -0300

    cambio a ver si anda de una vez

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,26 @@
 import cors from "cors";
 import express from "express";
-import { router } from "./routes/ventas.routes"
-const PORT = Number(process.env.PORT) || 3000;
-
+import { router } from "./routes/ventas.routes";
 
 const app = express();
-app.use(express.json());
-app.use(cors());
 
+const PORT = process.env.PORT
+    ? Number(process.env.PORT)
+    : 3000;
+
+app.use(express.json());
 app.use(cors({
     origin: [
-        'http://localhost:5173',
-        'https://app-helados.vercel.app'
+        "http://localhost:5173",
+        "https://app-helados.vercel.app",
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
 }));
-app.use("/", router)
 
+app.use("/", router);
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Servidor corriendo en puerto ${PORT}`);
+    console.log(`🚀 Servidor escuchando en puerto ${PORT}`);
 });
